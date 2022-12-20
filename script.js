0c4e8d158d45a86988c638470c88f293a72cbf58
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-12-20 10:34:01 +0100

    validating input and adding breakpoints

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function changeName(){
     const title = document.getElementById("title");
-    const name = document.getElementById("user-name");
+    const name = document.getElementById("name-input");
     const button = document.getElementById("submit-name");
+    const welcome = document.querySelector(".welcome");
+    const validate = document.querySelector(".valid-input");
 
-    title.innerHTML = "Hello " + name.value + "!";
-    name.style.display = "none";
-    button.style.display = "none";
+    
+
+    //validate the name and make sure the user has entered a name
+    if(name.value == ""){
+        validate.style.display = "block";
+    }else{
+        // Change the welcome message to include the name of the user
+        welcome.innerHTML = "Hello " + name.value + "!";
+        name.style.display = "none";
+        title.style.display = "none";
+        button.style.display = "none";
+        validate.style.display = "none";
+    }
+
 
 }
