aafdba65d551b7fbefaf5390f860765615e5be42
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-12-23 16:25:18 +0100

    just finished time function

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function changeName(){
         info_list.style.display = "block";
     }
 } 
-//accepting rules
+
 
 // adding the check icon
 const spans = document.querySelectorAll('.step-item');
@@ -37,7 +37,9 @@ function check(){
 }
 
 //set Options
-let currentIndex = 0;
+let currentIndex  = 0;
+let right_answers = 0;
+let countdowninterval;
 
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
@@ -46,20 +48,49 @@ function getQuestions(){
             // console.log(this.responseText);
             let myData = JSON.parse(this.responseText);
             let questionsCount = myData.length;
+
+            //make questions random
+            let randomQuestions = [];
+            for (let i = 0; i < questionsCount; i++) {
+                let randomIndex = Math.floor(Math.random() * myData.length);
+                randomQuestions.push(myData[randomIndex]);
+                myData.splice(randomIndex, 1);
+            }
             
             //Create Bullets + set questions count
             createBullets(questionsCount);
             //fetch the questions
-            fetchQuestions(myData[currentIndex], questionsCount);
+            fetchQuestions(randomQuestions[currentIndex], questionsCount);
 
+            //start countdown
+            countdown(5, questionsCount);
             // click on submit button
             submit_button.onclick = function(){
                 // get right answer
-                let right_answer = myData[currentIndex].correct_answer;
+                let right_answer = randomQuestions[currentIndex].correct_answer;
+                
                 // get user answer
                 checkAnswer(right_answer,questionsCount );
+                
                 //increment the current question
                 currentIndex++;
+
+                //remove the old question
+                quizz_area.innerHTML = '';
+                answers_area.innerHTML = '';
+
+                //fetch next questions
+                fetchQuestions(randomQuestions[currentIndex], questionsCount);
+
+                //handle bullets class on
+                handleBullets();
+
+                //start countdown again
+                clearInterval(countdowninterval);
+                countdown(5, questionsCount);
+
+                //show results
+                showResults(questionsCount);
             }
         }
     };
@@ -90,51 +121,131 @@ let answers_area = document.querySelector('.answers-area');
 //get questions from json file
 function fetchQuestions(obj, count){
 
-    //set the question title
-    let questionTitle = document.createElement('h2');
-    //set the question title text
-    let questionText = document.createTextNode(obj['title']);
-    //append the text to the title
-    questionTitle.appendChild(questionText);
-    //append the title to the quizz area
-    quizz_area.appendChild(questionTitle);
-
-    //create the answers
-    for (let i = 1; i <=4 ; i++) {
-        //create the main div
-        let answerDiv = document.createElement('div');
-        //add class to the main div
-        answerDiv.className = 'answer';
-        //create the radio input
-        let answerRadio = document.createElement('input');
-        answerRadio.type = 'radio';
-        answerRadio.name = 'question';
-        answerRadio.id = 'answer_' + i;
-        answerRadio.dataset.answer = obj['answer_' + i];
-
-        if (i == 1) {
-            answerRadio.checked = true;
+if (currentIndex < count){
+        //set the question title
+        let questionTitle = document.createElement('h2');
+        //set the question title text
+        let questionText = document.createTextNode(obj['title']);
+        //append the text to the title
+        questionTitle.appendChild(questionText);
+        //append the title to the quizz area
+        quizz_area.appendChild(questionTitle);
+    
+        //create the answers
+        for (let i = 1; i <=4 ; i++) {
+            //create the main div
+            let answerDiv = document.createElement('div');
+            //add class to the main div
+            answerDiv.className = 'answer';
+            //create the radio input
+            let answerRadio = document.createElement('input');
+            answerRadio.type = 'radio';
+            answerRadio.name = 'question';
+            answerRadio.id = 'answer_' + i;
+            answerRadio.dataset.answer = obj['answer_' + i];
+    
+            if (i == 1) {
+                answerRadio.checked = true;
+            }
+    
+            //create the label
+            let answerLabel = document.createElement('label');
+            answerLabel.htmlFor = 'answer_' + i;
+            //create the label text
+            let answerLabelText = document.createTextNode(obj['answer_' + i]);
+            //append the text to the label
+            answerLabel.appendChild(answerLabelText);
+    
+            //append the radio and label to the main div
+            answerDiv.appendChild(answerRadio);
+            answerDiv.appendChild(answerLabel);
+            
+            //append the main div to the answers area
+            answers_area.appendChild(answerDiv);
         }
-
-        //create the label
-        let answerLabel = document.createElement('label');
-        answerLabel.htmlFor = 'answer_' + i;
-        //create the label text
-        let answerLabelText = document.createTextNode(obj['answer_' + i]);
-        //append the text to the label
-        answerLabel.appendChild(answerLabelText);
-
-        //append the radio and label to the main div
-        answerDiv.appendChild(answerRadio);
-        answerDiv.appendChild(answerLabel);
-        
-        //append the main div to the answers area
-        answers_area.appendChild(answerDiv);
-    }
-
+    
 }
 
+}
+var user_answers = [];
 function checkAnswer(correct ,count ){
-    console.log(correct);
-    console.log(count);
+    let answers = document.getElementsByName('question');
+    let chosen_answer;
+    for (let i = 0; i < answers.length; i++) {
+        if(answers[i].checked){
+            user_answers.push(answers[i].dataset.answer);
+            chosen_answer = answers[i].dataset.answer;
+        }
+        
+    }
+    // console.log(`right anser is: ${correct}`)
+    // console.log(`your right answer is: ${chosen_answer}`)
+
+    if(correct === chosen_answer){
+        right_answers++;
+        console.log("Good Answer");
+    }
+    // console.log(right_answers);
+
 }
+
+function handleBullets(){
+    let bullets = document.querySelectorAll('.bullets .spans span');
+    let arrayOfBullets = Array.from(bullets);
+    arrayOfBullets.forEach((span, index)=>{
+        if(index === currentIndex){
+            span.className = 'on';
+        }
+    })
+}
+
+const bullet_and_time = document.querySelector('.bullets');
+const result = document.querySelector('.result');
+const quiz_app = document.querySelector('.quiz-app');
+function showResults(count){
+    let theResults;
+    if (currentIndex == count){
+        
+        quiz_app.remove();
+        if (right_answers > (count / 2) && (right_answers < count)) {
+            theResults = `<span class="good">GOOD</span>. You got ${right_answers} from ${count} questions`;
+        }else if(right_answers == count){
+            theResults = `<span class="perfect">PERFECT</span>. You Answered ALL questions Correctly`;   
+        }else{
+            theResults = `<span class="bad">BAD</span>. You got ${right_answers} from ${count} questions`;
+        }
+        result.innerHTML = theResults;
+        spans[1].classList.add('step-item-active');
+        spans[2].classList.add('step-item-active');
+        let quiz_icon = document.getElementById("quiz_icon");
+        quiz_icon.innerHTML = `<i class="bi bi-check-circle"></i>`;
+        let result_icon = document.getElementById("result_icon");
+        result_icon.innerHTML = `<i class="bi bi-check-circle"></i>`;
+
+        console.log(user_answers);
+    }
+}
+
+// countdown timer
+let countdownElement = document.querySelector('.countdown');
+function countdown(duration, count){
+    if (currentIndex < count){
+        let minutes, seconds;
+        countdowninterval = setInterval(function(){
+            minutes = parseInt(duration / 60);
+            seconds = parseInt(duration % 60);
+    
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+    
+            countdownElement.innerHTML = minutes + ':' + seconds;
+    
+            if (--duration < 0) {
+                clearInterval(countdowninterval);
+                submit_button.click();
+            }
+        }, 1000)
+    }
+}
+
+// add sound when seconds are less than
